23dc2d1df96d8cbab1762504fd9cfcae634cd365
Jelizaveta Lemeševa <jelizaveta.lemeseva@vinted.com>
2023-12-10 18:51:52 +0200

    Add test for multiple writes

--- test/simon_test.exs
+++ test/simon_test.exs
@@ -2,7 +2,47 @@ defmodule SimonTest do
   use ExUnit.Case, async: false
   doctest Simon
 
-  test "should read and write" do
+  # test "should read and write" do
+  #   :ok = LocalCluster.start()
+  #   [n1] = LocalCluster.start_nodes("primary", 1)
+
+  #   Process.sleep(500)
+
+  #   [n2, n3, n4, n5, n6] = LocalCluster.start_nodes("backup", 5)
+
+  #   Process.sleep(3000)
+
+  #   Simon.Client.write("a")
+
+  #   v = Simon.Client.get_log()
+  #   assert length(v) == 7
+
+  #   Simon.Client.write("b")
+
+  #   v = Simon.Client.get_log()
+  #   assert length(Enum.at(v, 1)) == 2
+
+  #   v = Simon.Client.get_storage()
+  #   assert v == [[], ["a", "b"], ["a"], ["a"], ["a"], ["a"], ["a"]]
+
+  #   Simon.Client.reset(n4)
+
+  #   Process.sleep(300)
+
+  #   Simon.Client.write("c")
+
+  #   v = Simon.Client.get_log()
+  #   [entry1, entry2, {{:write, v}, _pid, req_num, _ref}] = Enum.at(v, 3) # n4
+  #   assert v == "c"
+  #   assert req_num == 3
+
+  #   v = Simon.Client.get_storage()
+  #   assert v == [[], ["a", "b", "c"], ["a", "b"], ["a", "b"], ["a", "b"], ["a", "b"], ["a", "b"]]
+
+  #   LocalCluster.stop_nodes([n1, n2, n3, n4, n5, n6])
+  # end
+
+  test "should write in order" do
     :ok = LocalCluster.start()
     [n1] = LocalCluster.start_nodes("primary", 1)
 
@@ -13,42 +53,22 @@ defmodule SimonTest do
     Process.sleep(3000)
 
     Simon.Client.write("a")
-
-    v = Simon.Client.get_log()
-    assert length(v) == 7
-
     Simon.Client.write("b")
-
-    v = Simon.Client.get_log()
-    assert length(Enum.at(v, 1)) == 2
-
-    v = Simon.Client.get_storage()
-    assert v == [[], ["a", "b"], ["a"], ["a"], ["a"], ["a"], ["a"]]
-
-    Simon.Client.reset(n4)
-
-    Process.sleep(300)
-    # LocalCluster.stop_nodes([n4])
-
-    # [n4] = LocalCluster.start_nodes("simon", 1)
-
     Simon.Client.write("c")
+    Simon.Client.write("d")
+    Simon.Client.write("e")
+
+    Process.sleep(1000)
 
     v = Simon.Client.get_log()
-    [entry1, entry2, {{:write, v}, _pid, req_num, _ref}] = Enum.at(v, 3) # n4
-    assert v == "c"
-    assert req_num == 3
+    assert length(Enum.at(v, 1)) == 5
+    [entry1, entry2, entry3, entry4, {{:write, v}, _pid, req_num, _ref}] = Enum.at(v, 4) # n5
+    assert v == "e"
+    assert req_num == 5
 
     v = Simon.Client.get_storage()
-    assert v == [[], ["a", "b", "c"], ["a", "b"], ["a", "b"], ["a", "b"], ["a", "b"], ["a", "b"]]
+    assert v == [[], ["a", "b", "c", "d", "e"], ["a", "b", "c", "d"], ["a", "b", "c", "d"], ["a", "b", "c", "d"], ["a", "b", "c", "d"], ["a", "b", "c", "d"]]
 
-    # assert Node.ping(n4) == :pong
-
-    # Simon.Node.write("c")
-
-    # v = Simon.Node.read()
-    # assert v == "c"
-
-    LocalCluster.stop_nodes([n2, n3])
+    LocalCluster.stop_nodes([n1, n2, n3, n4, n5, n6])
   end
 end
