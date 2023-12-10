8492be8a4fe07fa9fdb1da58c24f86a35d00ebe5
Jelizaveta Lemeševa <jelizaveta.lemeseva@vinted.com>
2023-12-10 18:52:20 +0200

    Uncomment test

--- test/simon_test.exs
+++ test/simon_test.exs
@@ -2,45 +2,45 @@ defmodule SimonTest do
   use ExUnit.Case, async: false
   doctest Simon
 
-  # test "should read and write" do
-  #   :ok = LocalCluster.start()
-  #   [n1] = LocalCluster.start_nodes("primary", 1)
+  test "should read and write" do
+    :ok = LocalCluster.start()
+    [n1] = LocalCluster.start_nodes("primary", 1)
 
-  #   Process.sleep(500)
+    Process.sleep(500)
 
-  #   [n2, n3, n4, n5, n6] = LocalCluster.start_nodes("backup", 5)
+    [n2, n3, n4, n5, n6] = LocalCluster.start_nodes("backup", 5)
 
-  #   Process.sleep(3000)
+    Process.sleep(3000)
 
-  #   Simon.Client.write("a")
+    Simon.Client.write("a")
 
-  #   v = Simon.Client.get_log()
-  #   assert length(v) == 7
+    v = Simon.Client.get_log()
+    assert length(v) == 7
 
-  #   Simon.Client.write("b")
+    Simon.Client.write("b")
 
-  #   v = Simon.Client.get_log()
-  #   assert length(Enum.at(v, 1)) == 2
+    v = Simon.Client.get_log()
+    assert length(Enum.at(v, 1)) == 2
 
-  #   v = Simon.Client.get_storage()
-  #   assert v == [[], ["a", "b"], ["a"], ["a"], ["a"], ["a"], ["a"]]
+    v = Simon.Client.get_storage()
+    assert v == [[], ["a", "b"], ["a"], ["a"], ["a"], ["a"], ["a"]]
 
-  #   Simon.Client.reset(n4)
+    Simon.Client.reset(n4)
 
-  #   Process.sleep(300)
+    Process.sleep(300)
 
-  #   Simon.Client.write("c")
+    Simon.Client.write("c")
 
-  #   v = Simon.Client.get_log()
-  #   [entry1, entry2, {{:write, v}, _pid, req_num, _ref}] = Enum.at(v, 3) # n4
-  #   assert v == "c"
-  #   assert req_num == 3
+    v = Simon.Client.get_log()
+    [entry1, entry2, {{:write, v}, _pid, req_num, _ref}] = Enum.at(v, 3) # n4
+    assert v == "c"
+    assert req_num == 3
 
-  #   v = Simon.Client.get_storage()
-  #   assert v == [[], ["a", "b", "c"], ["a", "b"], ["a", "b"], ["a", "b"], ["a", "b"], ["a", "b"]]
+    v = Simon.Client.get_storage()
+    assert v == [[], ["a", "b", "c"], ["a", "b"], ["a", "b"], ["a", "b"], ["a", "b"], ["a", "b"]]
 
-  #   LocalCluster.stop_nodes([n1, n2, n3, n4, n5, n6])
-  # end
+    LocalCluster.stop_nodes([n1, n2, n3, n4, n5, n6])
+  end
 
   test "should write in order" do
     :ok = LocalCluster.start()
